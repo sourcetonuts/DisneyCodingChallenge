b52a3883d0b68cda225b0b9da4f8994ebece57a0
kenny <kenny@sourcetonuts.com>
2020-12-12 13:25:06 -0500

    added date picker change

--- CodingChallenge/MainPage.xaml.cs
+++ CodingChallenge/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CodingChallenge.ViewModels;
+using System;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -11,10 +12,24 @@ namespace CodingChallenge
     public sealed partial class MainPage 
         : Page
     {
+        TodayScoreViewModel _viewmodel;
         public MainPage()
         {
             this.InitializeComponent();
-            this.DataContext = new TodayScoreViewModel();
+
+            _viewmodel = new TodayScoreViewModel();
+            gamedayPicker.Date = new DateTime(2016, 5, 20);
+            gamedayPicker.DateChanged += GamedayPicker_DateChanged;
+
+            this.DataContext = _viewmodel;
+
+            Loaded += (s,e) => scoresListView.Focus(Windows.UI.Xaml.FocusState.Keyboard);
+        }
+
+        private void GamedayPicker_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            _viewmodel.LoadScores(args.NewDate.Value.Year, args.NewDate.Value.Month, args.NewDate.Value.Day);
+            scoresListView.Focus( Windows.UI.Xaml.FocusState.Keyboard );
         }
     }
 }
